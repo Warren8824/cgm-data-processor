8cfc1ed2a159fadca1c25a1e5d3532a0167f7914
Warren8824 <warrenbebbington88@gmail.com>
2025-01-12 15:06:20 +0000

    Add CarbsProcessor class to processors/__init__.py

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -1,13 +1,14 @@
 """Initialize processors package and register all processors."""
 
 from src.processors.base import DataProcessor
+from src.processors.carbs import CarbsProcessor
 from src.processors.cgm import CGMProcessor
 
 # from src.processors.insulin import InsulinProcessor
-# from src.processors.carbs import CarbsProcessor
 # from src.processors.notes import NotesProcessor
 
 __all__ = [
     "DataProcessor",
     "CGMProcessor",
+    "CarbsProcessor",
 ]
