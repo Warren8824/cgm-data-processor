675215f886c580c7d19351981e1d0086dc9d7fc7
Warren8824 <warrenbebbington88@gmail.com>
2025-01-12 14:40:22 +0000

    Register processors in __init__ file.

---
+++ src/processors/__init__.py
@@ -0,0 +1,13 @@
+"""Initialize processors package and register all processors."""
+
+from src.processors.base import DataProcessor
+from src.processors.cgm import CGMProcessor
+
+# from src.processors.insulin import InsulinProcessor
+# from src.processors.carbs import CarbsProcessor
+# from src.processors.notes import NotesProcessor
+
+__all__ = [
+    "DataProcessor",
+    "CGMProcessor",
+]
