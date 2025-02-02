15960e5d874afed7c92cb0433a16e447311f3f3b
Warren8824 <warrenbebbington88@gmail.com>
2025-02-02 14:04:28 +0000

    Add BGMProcessor to registry.

--- src/processors/__init__.py
+++ src/processors/__init__.py
@@ -1,6 +1,7 @@
 """Initialize processors package and register all processors."""
 
 from src.processors.base import DataProcessor
+from src.processors.bgm import BGMProcessor
 from src.processors.carbs import CarbsProcessor
 from src.processors.cgm import CGMProcessor
 from src.processors.insulin import InsulinProcessor
@@ -9,6 +10,7 @@ from src.processors.notes import NotesProcessor
 __all__ = [
     "DataProcessor",
     "CGMProcessor",
+    "BGMProcessor",
     "CarbsProcessor",
     "InsulinProcessor",
     "NotesProcessor",
