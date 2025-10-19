6ab4a33b4cf65c55248e6ae0bac8bb588ae3127a
Warren8824 <warrenbebbington88@gmail.com>
2025-10-19 16:34:08 +0100

    Add initial test coverage for exceptions.py

---
+++ tests/test_core/test_exceptions.py
@@ -0,0 +1,104 @@
+"""Unit tests for custom exception classes in src.core.exceptions.py"""
+
+import pytest
+
+from src.core.exceptions import (
+    AlignmentError,
+    CalibrationError,
+    CGMProcessorError,
+    DataExistsError,
+    DataGapError,
+    DataProcessingError,
+    DataQualityError,
+    DataValidationError,
+    DeviceFormatError,
+    ExportError,
+    FileAccessError,
+    FileError,
+    FileExtensionError,
+    FileParseError,
+    FormatDetectionError,
+    FormatError,
+    FormatLoadingError,
+    FormatValidationError,
+    MetricCalculationError,
+    ProcessingError,
+    ReaderError,
+    TimeAlignmentError,
+    TimestampProcessingError,
+    UnitConversionError,
+    ValidationError,
+)
+
+
+def test_base_exception_initialization():
+    exc = CGMProcessorError("Test error", {"foo": "bar"})
+    assert exc.details == {"foo": "bar"}
+
+
+def test_base_exception_details_default():
+    exc = CGMProcessorError("Test error")
+    assert exc.details == {}
+
+
+@pytest.mark.parametrize(
+    "exception_class",
+    [
+        FileError,
+        FileAccessError,
+        FileExtensionError,
+        FileParseError,
+        FormatError,
+        FormatDetectionError,
+        FormatLoadingError,
+        FormatValidationError,
+        DeviceFormatError,
+        ReaderError,
+        ProcessingError,
+        DataProcessingError,
+        TimestampProcessingError,
+        AlignmentError,
+        DataExistsError,
+        ValidationError,
+        DataValidationError,
+        ExportError,
+        DataQualityError,
+        TimeAlignmentError,
+        UnitConversionError,
+        MetricCalculationError,
+        CalibrationError,
+        DataGapError,
+    ],
+)
+def test_subclass_is_instance_of_base(exception_class):
+    """Test that each exception subclass is an instance of CGMProcessorError"""
+    exc = exception_class("Test")
+    assert isinstance(exc, CGMProcessorError)
+
+
+@pytest.mark.parametrize(
+    "subclass, parent",
+    [
+        (FileAccessError, FileError),
+        (FileExtensionError, FileError),
+        (FileParseError, FileError),
+        (FormatDetectionError, FormatError),
+        (FormatLoadingError, FormatError),
+        (FormatValidationError, FormatError),
+        (DeviceFormatError, FormatError),
+        (DataProcessingError, ProcessingError),
+        (TimestampProcessingError, ProcessingError),
+        (AlignmentError, ProcessingError),
+        (DataQualityError, ProcessingError),
+        (TimeAlignmentError, ProcessingError),
+        (UnitConversionError, ProcessingError),
+        (MetricCalculationError, ProcessingError),
+        (CalibrationError, ProcessingError),
+        (DataGapError, DataQualityError),
+        (DataExistsError, FileError),
+        (DataValidationError, ValidationError),
+    ],
+)
+def test_subclass_correct_parent(subclass, parent):
+    """Test that each subclass has the correct parent class"""
+    assert issubclass(subclass, parent)
